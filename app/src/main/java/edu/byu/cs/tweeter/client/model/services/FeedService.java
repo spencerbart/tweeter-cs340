00e392cf5ce81855799b2fa615e53ce3e4bb84bd
Spencer Bartholomew <38776747+spencerbart@users.noreply.github.com>
2023-10-18 00:41:41 -0600

    get feed working

--- app/src/main/java/edu/byu/cs/tweeter/client/model/services/FeedService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/services/FeedService.java
@@ -39,7 +39,7 @@ public class FeedService {
         public void handleMessage(@NonNull Message msg) {
             boolean success = msg.getData().getBoolean(GetFeedTask.SUCCESS_KEY);
             if (success) {
-                List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetFeedTask.STATUSES_KEY);
+                List<Status> statuses = (List<Status>) msg.getData().getSerializable(GetFeedTask.ITEMS_KEY);
                 boolean hasMorePages = msg.getData().getBoolean(GetFeedTask.MORE_PAGES_KEY);
                 assert statuses != null;
                 Status lastStatus = (statuses.size() > 0) ? statuses.get(statuses.size() - 1) : null;
