54b6dd1848a16012f4f42d72cf33efe983624e59
Spencer Bartholomew <38776747+spencerbart@users.noreply.github.com>
2023-10-18 00:27:30 -0600

    working follower and following count

--- app/src/main/java/edu/byu/cs/tweeter/client/model/services/backgroundTask/FollowTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/services/backgroundTask/FollowTask.java
@@ -26,48 +26,4 @@ public class FollowTask extends AuthenticatedTask {
     protected void runTask() {
         sendSuccessMessage();
     }
-
-//    @Override
-//    public void run() {
-//        try {
-//
-//            sendSuccessMessage();
-//
-//        } catch (Exception ex) {
-//            Log.e(LOG_TAG, ex.getMessage(), ex);
-//            sendExceptionMessage(ex);
-//        }
-//    }
-
-//    private void sendSuccessMessage() {
-//        Bundle msgBundle = new Bundle();
-//        msgBundle.putBoolean(SUCCESS_KEY, true);
-//
-//        Message msg = Message.obtain();
-//        msg.setData(msgBundle);
-//
-//        messageHandler.sendMessage(msg);
-//    }
-
-//    private void sendFailedMessage(String message) {
-//        Bundle msgBundle = new Bundle();
-//        msgBundle.putBoolean(SUCCESS_KEY, false);
-//        msgBundle.putString(MESSAGE_KEY, message);
-//
-//        Message msg = Message.obtain();
-//        msg.setData(msgBundle);
-//
-//        messageHandler.sendMessage(msg);
-//    }
-
-//    private void sendExceptionMessage(Exception exception) {
-//        Bundle msgBundle = new Bundle();
-//        msgBundle.putBoolean(SUCCESS_KEY, false);
-//        msgBundle.putSerializable(EXCEPTION_KEY, exception);
-//
-//        Message msg = Message.obtain();
-//        msg.setData(msgBundle);
-//
-//        messageHandler.sendMessage(msg);
-//    }
 }
